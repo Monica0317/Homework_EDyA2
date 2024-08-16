dd1f4ed783be3a47241d9afca2aaac16514bcc57
Monica <monicachicangana0317@gmail.com>
2024-08-16 10:08:48 -0500

    flecha y tradicional

---
+++ funcion flecha y tradicional-EDA2.js
@@ -0,0 +1,15 @@
+//Mónica Chicangana
+// Función tradicional
+function saludar(nombre) {
+    return "Hola, " + nombre + "!";
+}
+
+// Uso de la función
+console.log(saludar("Ana")); 
+
+//función flecha
+const saludar = (nombre) => {
+    return `Hola, ${nombre}!`;
+}
+// Uso de la función
+console.log(saludar("Ana"));
