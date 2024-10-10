fdb8f86da3a0c9cc8ba71eefc1b3320f497a7ea5
Monica <monicachicangana0317@gmail.com>
2024-10-09 23:45:59 -0500

    addCategories

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import FirstApp from './FirstApp.jsx'
+import { ComponentApp } from './ComponentApp.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FirstApp value={0}/>
+    <ComponentApp/>
   </StrictMode>,
 )
