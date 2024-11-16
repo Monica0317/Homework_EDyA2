86ff74c8d56e0e14669e9b23f65ace6c6b499697
Monica <monicachicangana0317@gmail.com>
2024-11-16 12:08:23 -0500

    Challenge contador con redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,12 @@
-import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-
-import HomePage from "./paginas/HomePage";
-import AboutPage from "./paginas/AboutPage";
-import LoginPage from "./paginas/LoginPage";
-import PrivatePage from "./paginas/PrivatePage";
-import NavComponent from "./components/ComponenteNav";
-
-import PrivateRoute from "./util/PrivateRoutes";
-import { useAuth } from "./Context/AuthContext";
+import React from "react";
+import Counter from "./counter/counter";
 
 const App = () => {
   return (
-    <>
-      <NavComponent />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/login" element={<LoginPage />} />
-
-        <Route element={<PrivateRoute />}>
-          <Route path="/private" element={<PrivatePage />} />
-        </Route>
-      </Routes>
-    </>
+    <div>
+      <Counter />
+    </div>
   );
 };
 
-const AppWithRouter = () => {
-  const location = useLocation();
-  const { dispatch } = useAuth();
-
-  useEffect(() => {
-    if (location.pathname !== "/login") {
-      dispatch({ type: "SET_LAST_PAGE", payload: location.pathname });
-      return;
-    }
-  }, [location.pathname, dispatch]);
-
-  return <App />;
-};
-
-const MainApp = () => (
-  <Router>
-    <AppWithRouter />
-  </Router>
-);
-
-export default MainApp;
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; 
-import "./index.css"; 
-import App from "./App.jsx"; 
-import { AuthProvider } from "./Context/AuthContext";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 
-// Renderizar la aplicación
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
